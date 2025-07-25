b1ddd09ed7b32030b08669085fdb304bd0316a64
vxbuilds <veektorhadex@gmail.com>
2025-07-25 18:36:50 -0400

    Changed weather imput variable name

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,4 +1,4 @@
-weather = input("What's the weather like today (sunny/rainy/cold):")
+weather_input = input("What's the weather like today (sunny/rainy/cold):")
 
 if weather == "sunny":
   print("Wear a t-shirt and sunglasses.")
