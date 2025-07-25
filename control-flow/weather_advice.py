a006fc43b2d933f1317979daae49aafc5efb528f
vxbuilds <veektorhadex@gmail.com>
2025-07-25 18:52:11 -0400

    Renamed Weather Input variable

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,10 +1,10 @@
-weather_input = input("What's the weather like today (sunny/rainy/cold):")
+current_weather = input("What's the weather like today (sunny/rainy/cold):")
 
-if weather == "sunny":
+if current_weather == "sunny":
   print("Wear a t-shirt and sunglasses.")
-elif weather == "rainy":
+elif current_weather == "rainy":
   print("Don't forget your umbrella and a raincoat.")
-elif weather == "cold":
+elif current_weather == "cold":
   print("Make sure to wear a warm coat and a scarf.")
 else:
   print("Sorry, I don't have recommendations for this weather.")
